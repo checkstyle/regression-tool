264198ae6a65a02246583939e1b93db46ad8ae32
rnveach <rveach02@gmail.com>
2017-06-07 08:17:33 -0400

    minor: fixed invalid path in commit validation test

--- src/test/java/com/github/checkstyle/regression/internal/CommitValidationTest.java
+++ src/test/java/com/github/checkstyle/regression/internal/CommitValidationTest.java
@@ -17,7 +17,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ////////////////////////////////////////////////////////////////////////////////
 
-package src.test.java.com.github.checkstyle.regression.internal;
+package com.github.checkstyle.regression.internal;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
