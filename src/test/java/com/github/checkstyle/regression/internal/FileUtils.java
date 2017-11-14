cc017929d1b51bffc554d9aa1f4fdc9d1c772c01
rnveach <rveach02@gmail.com>
2017-11-14 13:00:31 -0500

    Issue #88: upgrade checkstyle version

---
+++ src/test/java/com/github/checkstyle/regression/internal/FileUtils.java
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2017 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package com.github.checkstyle.regression.internal;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+public final class FileUtils {
+    protected static final String LF_REGEX = "\n";
+
+    protected static final String CRLF_REGEX = "\\r\\n";
+
+    private FileUtils() {
+    }
+
+    /**
+     * Reads the contents of a file.
+     *
+     * @param file the file whose contents are to be read
+     * @return contents of the file with all {@code \r\n} replaced by {@code \n}
+     * @throws IOException if I/O exception occurs while reading
+     */
+    public static String readFile(File file) throws IOException {
+        return new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8).replaceAll(
+                CRLF_REGEX, LF_REGEX);
+    }
+}
